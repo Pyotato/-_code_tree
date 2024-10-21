ae20452a3c1a2f7f38881a9c315b3aa6f51e7004
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-21 11:35:22 +0900

    Title: 20대의 평균 나이, Time: 163ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241021/20대의 평균 나이/average-age-of-20.js
+++ 241021/20대의 평균 나이/average-age-of-20.js
@@ -1,11 +1,20 @@
 const fs = require('fs');
-const [...input] =fs.readFileSync(0).toString().trim().split('\n');
+const [...nums] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const twenties = input.filter(v=>(+v)<30 && (+v)>=20);
-const average = (strArr) => {
-    const sum = strArr.reduce((acc,curr)=>acc+=(+curr),0);
-    const avg =  sum/strArr.length;
-    return ((Math.floor(avg*100))/100).toFixed(2)
+const twenties = [];
+
+while(nums.length>0){
+    const num = +(nums.splice(0,1));
+    if(num<30 && num>=20){
+        twenties.push(num);
+    } else {
+        break;
+    }
+}
+
+const average = (strArr)=>{
+    const sum = strArr.reduce((acc,curr)=>acc+=curr,0);
+    return (Math.floor((sum/strArr.length)*100)/100).toFixed(2);
 }
 
 console.log(average(twenties));
