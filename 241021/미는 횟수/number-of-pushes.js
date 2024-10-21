3673e591536baf9b500bf96dacbbe98067651472
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-21 10:31:47 +0900

    Title: 미는 횟수, Time: 238ms, Memory: 21MB, Status: Passed - CodeTree

--- 241021/미는 횟수/number-of-pushes.js
+++ 241021/미는 횟수/number-of-pushes.js
@@ -1,16 +1,21 @@
 const fs = require('fs');
 let [A, B] = fs.readFileSync(0).toString().trim().split('\n');
 
-let n = 0;
-
-while(n<B.length){
-    if(A===B){
-        break;
+const printN = ()=>{
+    let n = 0;
+    while(n<B.length){
+        if(A===B){
+            console.log(n);
+            return;
+        }
+        const tempA = [...A];
+        const lastStr = tempA.pop();
+        A = [lastStr,...tempA].join('');
+        n++;
     }
-    const tempA = [...A];
-    const lastStr = tempA.pop();
-    A = [lastStr,...tempA].join('');
-    n++;
+    console.log(-1);
 }
 
-console.log(n);
+
+
+printN();
