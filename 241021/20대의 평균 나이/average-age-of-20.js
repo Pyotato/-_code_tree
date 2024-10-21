51eace846fc0781e607f5f9cc4cc684c8586593b
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-21 11:11:08 +0900

    Title: 20대의 평균 나이, Time: 169ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241021/20대의 평균 나이/average-age-of-20.js
+++ 241021/20대의 평균 나이/average-age-of-20.js
@@ -5,7 +5,7 @@ const twenties = input.filter(v=>(+v)<30);
 const average = (strArr) => {
     const sum = strArr.reduce((acc,curr)=>acc+=(+curr),0);
     const avg =  sum/strArr.length;
-    return ((Math.round(avg*100))/100).toFixed(2)
+    return ((Math.floor(avg*100))/100).toFixed(2)
 }
 
 console.log(average(twenties));
