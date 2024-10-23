09c09c8237da2606efb0d430b5fbf7fac286e95f
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-23 15:21:48 +0900

    Title: 나눗셈의 나머지, Time: 222ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241023/나눗셈의 나머지/remainder-of-division.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+let [a,b] = [...fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(v=>+v),...Array.from({length:8},()=>null)];
+
+const quotients = [];
+
+while(a>=1){
+    quotients.push(a%b);
+    a = Math.floor(a/b);
+}
+
+const quotientCount = quotients.reduce((acc,curr)=>{
+    if(acc[curr]==null){
+        acc[curr] = 1;
+    } else {
+        acc[curr] ++;
+    }
+    return acc;
+},{});
+
+const sumOfSquares = Object.values(quotientCount).reduce((acc,curr)=>{
+    acc+=curr**2;
+    return acc;
+},0);
+
+console.log(sumOfSquares);
