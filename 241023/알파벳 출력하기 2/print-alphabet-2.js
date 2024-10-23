972e7729a29c60af16cba7e60b80534b7c690523
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-23 14:35:32 +0900

    Title: 알파벳 출력하기 2, Time: 180ms, Memory: 14MB, Status: Passed - CodeTree

--- 241023/알파벳 출력하기 2/print-alphabet-2.js
+++ 241023/알파벳 출력하기 2/print-alphabet-2.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
 const N = +fs.readFileSync('/dev/stdin').toString().trim();
 
-let alpha = 'A'.charCodeAt(0);
+const A_CHAR_CODE = 'A'.charCodeAt(0);
 const Z_CHAR_CODE = 'Z'.charCodeAt(0);
+
+let alpha = A_CHAR_CODE;
 
 const alphabets = Array.from({length:N},(_,i)=>[...Array.from({length:i},()=>' '),...Array.from({length:N-i},(_,j)=>{
     const currentAlpha = alpha> Z_CHAR_CODE? 'Z':  String.fromCharCode(alpha++);
+    if(currentAlpha==='Z'){
+        alpha = A_CHAR_CODE;
+    }
     return currentAlpha;
 })].join(' '));
 
