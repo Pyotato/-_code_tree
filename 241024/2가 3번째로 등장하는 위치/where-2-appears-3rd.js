5aa0f6947f8351c434ab4f0dac33a0a704d35780
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-24 09:24:18 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 175ms, Memory: 14MB, Status: Passed - CodeTree

--- 241024/2가 3번째로 등장하는 위치/where-2-appears-3rd.js
+++ 241024/2가 3번째로 등장하는 위치/where-2-appears-3rd.js
@@ -6,7 +6,7 @@ const nums= numbers.split(' ').map(v=>+v);
 let count = 0;  // 2 등장 횟수 카운트
 let index = 0;  // 자릿수 카운트
 
-while(nums.length>0){
+while(nums.length>=0){
     const current = nums.splice(0,1)[0];
     if(count===3){
         console.log(index);
