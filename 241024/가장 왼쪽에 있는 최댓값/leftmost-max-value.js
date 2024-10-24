b4f13d1650768e9a7e449dbb945350b99baae3bd
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-24 14:38:15 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 190ms, Memory: 16MB, Status: Passed - CodeTree

--- 241024/가장 왼쪽에 있는 최댓값/leftmost-max-value.js
+++ 241024/가장 왼쪽에 있는 최댓값/leftmost-max-value.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
 const [_,strNums] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const numbers = strNums.split(' ').map(v => +v);
+let numbers = strNums.split(' ').map(v => +v);
 
 const answer = [];
 
 while(numbers.length>0){
     const maxIndex = numbers.indexOf(Math.max(...numbers));
-    answer.push(maxIndex+1);
-    numbers.splice(0, maxIndex);
     if(maxIndex+1===1){
+        answer.push(maxIndex+1);
         break;
     }
+    answer.push(maxIndex+1);
+    numbers = numbers.slice(0, maxIndex);
 }
 console.log(answer.join(' '));
