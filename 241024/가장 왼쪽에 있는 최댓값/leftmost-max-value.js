018a9d6421650fdd70455833b3447443aea878d8
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-24 14:35:01 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 208ms, Memory: 47MB, Status: Wrong Answer - CodeTree

---
+++ 241024/가장 왼쪽에 있는 최댓값/leftmost-max-value.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const [_,strNums] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const numbers = strNums.split(' ').map(v => +v);
+
+const answer = [];
+
+console.log(numbers);
+
+while(numbers.length>0){
+    const maxIndex = numbers.indexOf(Math.max(...numbers));
+    answer.push(maxIndex+1);
+    numbers.splice(0, maxIndex);
+    if(maxIndex+1===1){
+        break;
+    }
+}
+console.log(answer.join(' '));
