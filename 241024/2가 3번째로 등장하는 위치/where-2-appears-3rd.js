ad7f6367f2cc015b7e509a7523e6178517ee4386
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-24 09:22:39 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 168ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241024/2가 3번째로 등장하는 위치/where-2-appears-3rd.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const [n,numbers] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const nums= numbers.split(' ').map(v=>+v);
+
+let count = 0;  // 2 등장 횟수 카운트
+let index = 0;  // 자릿수 카운트
+
+while(nums.length>0){
+    const current = nums.splice(0,1)[0];
+    if(count===3){
+        console.log(index);
+        break;
+    }
+    if(current===2){
+        count++;
+    } 
+    index++;
+}
