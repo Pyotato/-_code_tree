30527fbd95388c43089132354bc1f21d06f69bf7
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-25 20:21:04 +0900

    Title: n개의 숫자 중 최소, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241025/n개의 숫자 중 최소/min-of-n-num.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const [N, nums] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const numbers = [...new Set(nums.split(' '))].map(v => +v);
+const minNum = Math.min(...numbers);
+
+
+const countAppearance = (target)=>{
+    const regex = new RegExp(`${target}`,'g');
+    return nums.split(' ').join(' ').match(regex).length;
+} 
+
+console.log(minNum, countAppearance(minNum));
