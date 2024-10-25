8fc655eaa617ea03dd3f2404823ec821b8d00bba
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-25 20:02:00 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 183ms, Memory: 15MB, Status: Passed - CodeTree

--- 241025/중복되지 않는 숫자 중 최대/max-of-unique-number.js
+++ 241025/중복되지 않는 숫자 중 최대/max-of-unique-number.js
@@ -4,10 +4,24 @@ const[N,nums] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const numbers = nums.split(' ').reduce((acc,curr)=>{
     if(!acc[curr]){
         acc[curr] = 1;
-    } 
+    } else {
+        acc[curr]++;
+    }
     return acc;
 },{});
 
 
-const maxNum = Math.max(...Object.keys(numbers).map(v=>+v));
-console.log(numbers[maxNum]>1 ? -1 : maxNum);
+const filtered = [];
+
+Object.keys(numbers).forEach(v=>{
+    if(numbers[v]===1){
+        filtered.push(v);
+    }
+})
+
+if(filtered.length>0){
+   const maxNum =  Math.max(...filtered);
+   console.log(maxNum);
+} else{
+    console.log(-1);
+}
