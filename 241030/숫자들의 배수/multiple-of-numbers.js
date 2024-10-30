2caa817cdb8b011a722efa835856049528dbdf8f
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-30 18:47:31 +0900

    Title: 숫자들의 배수, Time: 178ms, Memory: 47MB, Status: Passed - CodeTree

--- 241030/숫자들의 배수/multiple-of-numbers.js
+++ 241030/숫자들의 배수/multiple-of-numbers.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
-const n = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-const count = 10;
+const n = +(fs.readFileSync('/dev/stdin').toString().trim());
+
+let count = n%5===0?2: 10;
 const answer = Array.from({length:count},(_,i)=>n*(i+1));
 
-console.log(...answer);
+    console.log(...answer);
