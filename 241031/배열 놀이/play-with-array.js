9201b16cb30cb13f4fdda2a2110ef43f6a3b4ed3
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-31 12:05:15 +0900

    Title: 배열 놀이, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241031/배열 놀이/play-with-array.js
+++ 241031/배열 놀이/play-with-array.js
@@ -1,17 +1,21 @@
 const fs = require('fs');
-const [_,numbers,...queries] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const [nq,numbers,...queries] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const nums = numbers.split(' ');
+
+let [n,q] = nq.split(' ');
+const nums = numbers.trim().split(' ');
 
 while(queries.length>0){
     const [num,...others] = queries.splice(0,1)[0].split(' ');
-    if(num==1){
-        console.log(...others);
-    } else if(num==2){
-        const index = nums.findIndex(v => +v === +others)+1;
-        console.log(index<=0?0:index);
-    } else {
+    if(num == 1){
+        console.log(nums[+(others[0])-1]);
+    } 
+    else if(num == 2){
+        const index = nums.findIndex(v => +v === +others);
+        console.log(index<=0?0:index+1);
+    } 
+    else {
         const [s,e] = others;
-        console.log(...nums.slice(+(s)-1,+e));
+        console.log(...nums.slice(+(s)-1,(+e)));
     }
 }
