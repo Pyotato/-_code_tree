39b6328109d8b6921a8b95143967032409913679
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-31 11:13:47 +0900

    Title: 점수대 파악하기, Time: 236ms, Memory: 14MB, Status: Passed - CodeTree

--- 241031/점수대 파악하기/find-out-the-score-range.js
+++ 241031/점수대 파악하기/find-out-the-score-range.js
@@ -4,14 +4,15 @@ const numbers = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
 const scores = {};
 
 while(numbers.length>0){
-    const current = Math.floor((+(numbers.splice(0,1)[0]))/10)*10;
+    const current = +(numbers.splice(0,1)[0]);
     if(current===0){
         break;
     }
-    if(scores?.[current]==null){
-        scores[current] = 1;
+    const scoreRange =  Math.floor(current/10)*10;
+    if(scores?.[scoreRange]==null){
+        scores[scoreRange] = 1;
     } else {
-        scores[current]++;
+        scores[scoreRange]++;
     }
 }
 
