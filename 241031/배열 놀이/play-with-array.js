03deda2e755607942c27f50b55c51e273054db97
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-10-31 12:12:09 +0900

    Title: 배열 놀이, Time: 241ms, Memory: 14MB, Status: Passed - CodeTree

--- 241031/배열 놀이/play-with-array.js
+++ 241031/배열 놀이/play-with-array.js
@@ -11,8 +11,8 @@ while(queries.length>0){
         console.log(nums[+(others[0])-1]);
     } 
     else if(num == 2){
-        const index = nums.findIndex(v => +v === +others);
-        console.log(index<=0?0:index+1);
+        const index = nums.findIndex(v => +v === +others)+1;
+        console.log(index<=0?0:index);
     } 
     else {
         const [s,e] = others;
