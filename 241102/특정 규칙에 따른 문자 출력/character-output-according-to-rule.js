9f051f9c791cca8dec321201775b16503a8f6323
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-11-02 08:16:09 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 164ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241102/특정 규칙에 따른 문자 출력/character-output-according-to-rule.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const n = +(fs.readFileSync('/dev/stdin').toString().trim());
+
+let counter = 0;
+
+while(counter<n){
+    console.log(''.padStart(2*(n-(counter+1)),'  ')+''.padStart(2*(counter+1),'@ '));
+    counter++;
+}
+
+while(counter>0){
+    console.log(''.padStart(2*(counter-1),'@ ')+''.padStart(2*(n-(counter-1)),'  '));
+    counter--;
+}
