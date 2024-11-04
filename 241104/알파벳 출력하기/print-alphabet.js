b9e229b1e74c0d293b8654200b3429c87853a147
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-11-04 11:20:07 +0900

    Title: 알파벳 출력하기, Time: 181ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241104/알파벳 출력하기/print-alphabet.js
+++ 241104/알파벳 출력하기/print-alphabet.js
@@ -1,10 +1,18 @@
 const fs = require('fs');
 const n = +(fs.readFileSync('/dev/stdin').toString().trim());
 
-let count = 0;
-let startChar = 'A'.charCodeAt(0);
+let count = 0
+const A = 'A'.charCodeAt(0);
+const Z = 'Z'.charCodeAt(0);
+let startChar = A;
 
 while(count<n){
-    console.log(Array.from({length:count+1},()=>String.fromCharCode(startChar++)).join(''))
+    console.log(Array.from({length:count+1},()=>{
+        if(startChar-1===Z){
+            startChar = A;
+             return String.fromCharCode(startChar);
+        }
+        return String.fromCharCode(startChar++)
+        }).join(''))
     count++;
 }
