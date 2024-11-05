70ae4f67cb1db08d558060cde98304aa5595028b
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-11-05 11:34:55 +0900

    Title: 구구단 만들기, Time: 181ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241105/구구단 만들기/print-multiplication-table.js
+++ 241105/구구단 만들기/print-multiplication-table.js
@@ -3,8 +3,7 @@ const [a,b] = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
 
 let count = 1;
 
-
 while(count<=9){
     let j = count++;
-    console.log(Array.from({length:a===b?1:(+b-(+a))/2},(_,k)=>`${2*((+b)/2-k)} * ${j} = ${(2*((+b)/2-k))*j}`).join(' / '));
+    console.log(Array.from({length:a===b?1:(+b)/2},(_,k)=>`${2*((+b)/2-k)} * ${j} = ${(2*((+b)/2-k))*j}`).join(' / '));
 }
