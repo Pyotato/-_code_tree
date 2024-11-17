36f52adfc4767491580ffe611500cef3a15046e3
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-11-17 14:33:04 +0900

    Title: 홀수만 더하기, Time: 183ms, Memory: 15MB, Status: Passed - CodeTree

--- 241117/홀수만 더하기/add-only-odd-numbers.js
+++ 241117/홀수만 더하기/add-only-odd-numbers.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const ns = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
+//  n개
+ns.splice(0,1);
+
 let count = 0;
 
 while(ns.length>0){
     let n = +(ns.pop());
-    if(n%3===0 && n%2==1){
+    if(n%3===0 && n%2!==0){
         count+=n;
     }
 }
