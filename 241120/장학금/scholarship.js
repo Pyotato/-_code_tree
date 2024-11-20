b999bc6a753f8426f2cd9c675bc86eaf4f59a25d
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-11-20 10:19:18 +0900

    Title: 장학금, Time: 182ms, Memory: 15MB, Status: Passed - CodeTree

--- 241120/장학금/scholarship.js
+++ 241120/장학금/scholarship.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const [mid,final] = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(v=>+v);
 
 const scholarship = (mid,final)=>{
-    if(mid<=90){
+    if(mid<90){
         return 0;
     }
     if(final>=95){
