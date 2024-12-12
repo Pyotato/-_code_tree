4c8cf25062503fbcbaff0bebc51deacd3477d665
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-12-12 14:51:35 +0900

    Title: 함께가는 열차, Time: 401ms, Memory: 43MB, Status: Passed - CodeTree

--- 241212/함께가는 열차/a-train-that-goes-together.js
+++ 241212/함께가는 열차/a-train-that-goes-together.js
@@ -1,84 +1,19 @@
 const fs = require('fs');
-const [N,...locationSpeed] = fs.readFileSync(0).toString().trim().split('\n');
+const [n, ...locationSpeed] = fs.readFileSync(0).toString().trim().split('\n');
 
-const trains = locationSpeed.reduce((acc,curr,i)=>{
-    const [location,speed] = curr.split(' ').map(v=>+v);
-    acc[i+1] = {location,speed,joined:false};
-    return acc;
-},{});
+const trains = locationSpeed.map((v)=>v.split(' ').map(Number));
 
-const isJoined = (back,front)=>{
-    if(front<=back){
-        return true;
-    }
-    return false;
+let count = 1; 
+let speed = trains[n - 1][1]; // 마지막 열차 속도
+
+// 뒤에서 두번째 열차에서부터 첫 열차까지 
+for (let i = n - 2; i >= 0; i--) {
+  if (trains[i][1] > speed) {
+    trains[i][1] = speed; // 앞 열차 속도로 맞추기 
+  } else {
+    count++;
+    speed = trains[i][1]; // 현재 열차 속도 맞추기
+  }
 }
 
-const syncSpeed = ()=>{
-    const joinedSpeeds = [];
-    const joined = new Set();
-    for(let i=1;i<N;i++){
-        if(trains[i].joined && trains[i+1].joined){
-            joinedSpeeds.push(trains[i].speed);
-            joinedSpeeds.push(trains[i+1].speed);
-            joined.add(i);
-            joined.add(i+1);
-        }
-    }
-    const minSpeed = Math.min(...joinedSpeeds);
-    [...joined].forEach(key=>trains[key].speed = minSpeed);
-}
-
-const isAllJoined = ()=>{
-    return Object.values(trains).every((v)=>v.joined);
-}
-
-const canMeet = ()=>{
-    const isNotJoinedKeys = Object.keys(trains).filter(key=>!trains[key].joined);
-    const keys = [];
-    for(let i=0;i<isNotJoinedKeys.length;i++){
-        if(
-        // 연속되거나
-        +(isNotJoinedKeys[i])+1 === +isNotJoinedKeys[i+1] && 
-        // 속도가 다른
-        trains[isNotJoinedKeys[i]].speed != trains[isNotJoinedKeys[i+1]]?.speed){
-            keys.push(isNotJoinedKeys[i]);
-            keys.push(isNotJoinedKeys[i+1]);
-        }
-    }
-    return keys.length>0;
-}
-
-while(true){
-    Object.keys(trains).forEach((key)=>{
-        trains[key].location+=trains[key].speed;
-    });
-    for(let i=1;i<Object.keys(trains).length;i++){
-        if(isJoined(trains[i].location,trains[i+1].location)){
-            trains[i].joined = true;
-            trains[i+1].joined = true;
-            // 앞 속도에 맞춰 이동
-            syncSpeed();
-        }
-    } 
-    
-    // 모두 한 덩어리라면
-    if(isAllJoined()){
-        console.log(1);
-        break;
-    }
-
-    if(!canMeet()){
-      break;
-      
-    }
-}
-
-const joinedCount = Object.keys(trains).reduce((acc,curr)=>{
-    if(trains[+curr].joined !== trains[+curr+1]?.joined){
-        acc++;
-    }
-    return acc;
-},0);
-
-console.log(joinedCount);
+console.log(count);
