58689332046c5faa0091efa7b000fa690e174f3e
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-12-12 10:56:04 +0900

    Title: 십진수와 이진수, Time: 204ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241212/십진수와 이진수/decimal-and-binary-number.js
+++ 241212/십진수와 이진수/decimal-and-binary-number.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const N = fs.readFileSync(0).toString().trim();
 
-const num = parseInt(N,2);
+const num = Math.floor(parseInt(N, 2))*17;
 
-console.log((num*17).toString(2));
+console.log( parseInt(num, 2));
+console.log(num.toString(2));
