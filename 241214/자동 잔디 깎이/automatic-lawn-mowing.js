dee8e92f29e025dd0b1238977b0631087d7ff968
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-12-14 15:59:57 +0900

    Title: 자동 잔디 깎이, Time: 246ms, Memory: 18MB, Status: Passed - CodeTree

--- 241214/자동 잔디 깎이/automatic-lawn-mowing.js
+++ 241214/자동 잔디 깎이/automatic-lawn-mowing.js
@@ -1,49 +1,5 @@
-// const fs = require('fs');
-// const [N, ...moves] = fs.readFileSync(0).toString().trim().split('\n');
-
-// const directions = {
-//     "W":(d)=>[0,-d],
-//     "S":(d)=>[-d,0],
-//     "N":(d)=>[d,0],
-//     "E":(d)=>[0,d]
-// }
-
-// let grid = [[0,0,false]];
-
-// const revisited = (nx,ny)=>{
-//     const newGrid = [];
-//     let hasVisited = false;
-//     for(const [x,y,visited] of grid){
-//         if(nx===x && ny===y){
-//             newGrid.push([x,y,true]);
-//             hasVisited = true;
-//         } else{
-//             newGrid.push([x,y,visited]);
-//         }
-//     }
-//     grid = newGrid;
-//     return hasVisited;
-// }
-
-// moves.forEach((v)=>{
-//     const [direction, distance] = v.split(' ');
-//     const [x,y]= grid.pop();
-//     grid.push([x,y]);
-//     for(let i=1;i<=+distance;i++){
-//         const [moveX,moveY] = directions[direction](i);
-//         const [nx,ny] = [x+moveX,y+moveY];
-//         if(revisited(nx,ny)){
-//             grid.push([nx,ny,true]);
-//         } else {
-//             grid.push([nx,ny])
-//         }
-//     }
-// })
-
-// console.log(grid.filter(([_,__,revisited])=>revisited==true));
-
 const fs = require('fs');
-const [N, ...moves] = fs.readFileSync(0).toString().trim().split('\n');
+const [_, ...moves] = fs.readFileSync(0).toString().trim().split('\n');
 
 const directions = {
     "W":(d)=>[0,-d],
