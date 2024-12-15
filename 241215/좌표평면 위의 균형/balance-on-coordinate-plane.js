99354ed6cb140dc6e3412ff93c43b5735f974eb0
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-12-15 22:50:37 +0900

    Title: 좌표평면 위의 균형, Time: 191ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241215/좌표평면 위의 균형/balance-on-coordinate-plane.js
+++ 241215/좌표평면 위의 균형/balance-on-coordinate-plane.js
@@ -1,45 +1,107 @@
 const fs = require('fs');
 const [NR, ...xys] = fs.readFileSync(0).toString().trim().split('\n');
 
-const [_,R] = NR.split(' ').map(Number);
+const [N,R] = NR.split(' ').map(Number);
+const points = xys.map(v => v.split(' ').map(Number));
 
-const points =xys.map(v=>v.split(' ').map(Number));
-
-// 각 방면의 좌표 개수 차이
-const pointCountDif = (points)=>{
-    const max = Math.max(...points);
-    return points.reduce((acc,curr)=>{
-        acc+=Math.abs(curr-max);
-        return acc;
-    },0);
-}
-
-// 1,2,3,4 방면 중 위치 리턴
-const getLocation = (x,y,a,b)=>{
-    if(x>a){
-        return y<b?0:1;
-    }else{
-        return y<b?2:3;
+const getLocation = (x, y, centerX, centerY) => {
+    if (x > centerX) {
+        return y < centerY ? 0 : 1;
+    } else {
+        return y < centerY ? 2 : 3;
     }
-}
+};
+let meanX = 0;
+let meanY = 0;
+let [minX,maxX,minY,maxY] = points.reduce((acc,curr)=>{
+    acc[0] = Math.min(curr[0],acc[0]);
+    acc[1] = Math.max(curr[0],acc[1]);
+    acc[2] = Math.min(curr[1],acc[2]);
+    acc[3] = Math.max(curr[1],acc[3]);
+    meanX+=curr[0];
+    meanY+=curr[1];
+    return acc;
+},[Infinity,-Infinity,Infinity,-Infinity]);
+
+meanX= Math.round(meanX/N);
+meanY= Math.round(meanY/N);
+
+meanX= meanX%2===1 ?meanX+1:meanX;
+meanY= meanY%2===1 ?meanY+1:meanY;
+
+
+
+minX= minX%2===1 ?minX-1:minX;
+minY= minY%2===1 ?minY-1:minY;
+maxX= maxX%2===1 ?maxX+1:maxX;
+maxY= maxY%2===1 ?maxY+1:maxY;
+
+// console.log(minX,maxX,minY,maxY,meanX,meanY);
 
 let min = Infinity;
 let answer = -Infinity;
 
-for(let i=2;i<=R;i+=2){
-    for(let j=2;j<=R;j+=2){
-        const copy = [...points];
-        let distribution =[0,0,0,0];
-        while(copy.length>0){
-            const point = copy.pop();
-            distribution[getLocation(...point,i,j)]++;
-        }
-        const diff = pointCountDif(distribution);
-        if(min>diff){
-            min =diff
-            answer = Math.max(...distribution);
-        }
-    }
+const sumDistribution = (arr,max)=>{
+    return arr.reduce((acc, curr) => acc += Math.abs(curr - max), 0);
 }
 
-console.log(answer); 
+for(let i=meanX;i<=maxX;i+=2){
+    for(let j=meanY;j<=maxY;j+=2){
+        let distribution = [0, 0, 0, 0];
+        for (const [x, y] of points) {
+            const loc = getLocation(x, y, i, j);
+            distribution[loc]++;
+        }
+        const max = Math.max(...distribution);
+        const diff = sumDistribution(distribution,max);
+        if (diff < min) {
+            min = diff;
+            answer = max;
+        }
+    }
+    // for(let j=meanY;j>=minY;j-=2){
+    //     let distribution = [0, 0, 0, 0];
+    //     for (const [x, y] of points) {
+    //         const loc = getLocation(x, y, i, j);
+    //         distribution[loc]++;
+    //     }
+    //     const max = Math.max(...distribution);
+    //     const diff = sumDistribution(distribution,max);
+    //     if (diff < min) {
+    //         min = diff;
+    //         answer = max;
+    //     }
+    // }
+}
+
+// for(let i=meanX;i>=minX;i-=2){
+//     for(let j=meanY;j<=maxY;j+=2){
+//         let distribution = [0, 0, 0, 0];
+//         for (const [x, y] of points) {
+//             const loc = getLocation(x, y, i, j);
+//             distribution[loc]++;
+//         }
+//         const max = Math.max(...distribution);
+//         const diff = sumDistribution(distribution,max);
+//         if (diff < min) {
+//             min = diff;
+//             answer = max;
+//         }
+//     }
+//     for(let j=meanY;j>=minY;j-=2){
+//         let distribution = [0, 0, 0, 0];
+//         for (const [x, y] of points) {
+//             const loc = getLocation(x, y, i, j);
+//             distribution[loc]++;
+//         }
+//         const max = Math.max(...distribution);
+//         const diff = sumDistribution(distribution,max);
+//         if (diff < min) {
+//             min = diff;
+//             answer = max;
+//         }
+//     }
+// }
+
+
+console.log(answer);
