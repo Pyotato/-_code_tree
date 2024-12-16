cd1e5d2eabae13bd09af3f0b37bda6f6c1a5aadb
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-12-16 14:38:59 +0900

    Title: 수업태도 평가, Time: 171ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241216/수업태도 평가/class-attitude-assessment.js
+++ 241216/수업태도 평가/class-attitude-assessment.js
@@ -1,25 +1,27 @@
 const fs = require('fs');
-const [_, ...studentGrades] = fs.readFileSync(0).toString().trim().split('\n');
+const [_, ...studentNGrades] = fs.readFileSync(0).toString().trim().split('\n');
 const STUDENTS  = {
     "Bessie":0, "Elsie":0, "Daisy":0, "Gertie":0, "Annabelle":0, "Maggie":0, "Henrietta":0
 }
 
-studentGrades.forEach((stGr)=>{
+studentNGrades.forEach((stGr)=>{
     const [name,grade] = stGr.split(' ');
     STUDENTS[name]+=+grade;
 });
 
+const studentGrades = Object.values(STUDENTS);
+const studentNames = Object.keys(STUDENTS);
+const lowestGrade = Math.min(...studentGrades);
+
 const printSecondLowest = (STUDENTS)=>{
-    const studentNames = Object.values(STUDENTS);
-    if(studentNames.length===1){
-        return studentNames[0];
-    }
-    const lowestGrade = Math.min(...studentNames);
-    const filteredLowest = Object.keys(STUDENTS).filter(key => STUDENTS[key]!=lowestGrade);
+    const filteredLowest = studentNames.filter(key => STUDENTS[key]!=lowestGrade);
     const secondLowestStudents = filteredLowest.sort((a,b)=>filteredLowest[b]-filteredLowest[a]);
     const secondlowestGradeStudent = secondLowestStudents.pop();
     const secondlowestGrade = STUDENTS[secondlowestGradeStudent];
-    const secondlowestGradeStudents = Object.keys(STUDENTS).filter(key => STUDENTS[key]==secondlowestGrade);
+    const secondlowestGradeStudents = studentNames.filter(key => STUDENTS[key]==secondlowestGrade);
+    if(secondlowestGradeStudents.length===1){
+        return secondlowestGradeStudents[0];
+    }
     if(filteredLowest.length===0 || secondlowestGradeStudents.length>1){
         return 'Tie';
     }
