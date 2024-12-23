4946b6aafe1a7efc17ca7934d5bef4b9153f3bb2
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-12-23 13:29:46 +0900

    Title: 소수 4, Time: 180ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241223/소수 4/prime-number-4.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const [n,k] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+const MIN_PRIME = 2;
+
+// 15 12
+// 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+// p = 2,  3 5 7 9 11 13 15
+// p = 3,  5 7 11 13
+// p = 5, 7 11 13
+// p = 7, 11 13
+// p = 11, 13
+// p = 13
+
+const deleteKthPrime = (n,k)=>{
+    let nums = Array.from({length:n-MIN_PRIME+1},(_,i)=>MIN_PRIME+i);
+    let p = nums.shift();
+    k--;
+    if(k===0){
+        return p;
+    } else {
+        while(nums.length>0){
+            const filtered = nums.filter(v=>{
+                if(v%p===0){
+                    k--;
+                    if(k===0){
+                        p=v;
+                    }
+                }
+                return v%p!==0;
+            });
+            nums = filtered;
+            if(k===0){
+                return p;
+            } else {
+                p = nums.shift();
+                k--;
+            }
+        }
+    }
+    return p;
+
+}
+
+console.log(deleteKthPrime(n,k));
