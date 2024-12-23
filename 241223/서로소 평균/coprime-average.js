e5072d2544c216aacc72ba1bb6839a723f7f0e55
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-12-23 14:21:18 +0900

    Title: 서로소 평균, Time: 5077ms, Memory: 19MB, Status: Time Limit Exceed - CodeTree

--- 241223/서로소 평균/coprime-average.js
+++ 241223/서로소 평균/coprime-average.js
@@ -4,25 +4,24 @@ const [_,...aX] = fs.readFileSync(0).toString().trim().split('\n');
 let [a,...x] = aX;
 a = a.split(' ').map(Number);
 x = +x[0];
-// const getGCMSet = (n)=>{
-//     const gcmSet = new Set([1,n]);
-//     for(let i=2;i<=n/2;i++){
-//         if(n%i===0){
-//             gcmSet.add(i);
-//         }
-//     }
-//     return gcmSet;
-// }
-// // 서로소 : 1빼고는 공통 약수가 없음
-// const isCoprime = (setA,setB)=>{
-//     const filtered = [...setA].filter((v)=>setB.has(v));
-//     return filtered.length===1;
-// } 
 
-// const setB = getGCMSet(x);
-// const coprimes = a.filter(v=>isCoprime(getGCMSet(v),setB));
-// const sum = coprimes.reduce((acc,curr)=>acc+=curr,0);
+const getGCMSet = (n)=>{
+    const gcmSet = new Set([1,n]);
+    for(let i=2;i<=n/2;i++){
+        if(n%i===0){
+            gcmSet.add(i);
+        }
+    }
+    return gcmSet;
+}
+// 서로소 : 1빼고는 공통 약수가 없음
+const isCoprime = (setA,setB)=>{
+    const filtered = [...setA].filter((v)=>setB.has(v));
+    return filtered.length===1;
+} 
 
-// console.log((sum/coprimes.length).toFixed(2));
+const setB = getGCMSet(x);
+const coprimes = a.filter(v=>isCoprime(getGCMSet(v),setB));
+const sum = coprimes.reduce((acc,curr)=>acc+=curr,0);
 
-console.log(a,x)
+console.log((sum/coprimes.length).toFixed(2));
