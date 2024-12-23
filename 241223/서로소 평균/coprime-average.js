35786d3bfef723e00ffa60310f700ceb47d234a6
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-12-23 13:50:16 +0900

    Title: 서로소 평균, Time: 5094ms, Memory: 19MB, Status: Time Limit Exceed - CodeTree

---
+++ 241223/서로소 평균/coprime-average.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const [_,...aX] = fs.readFileSync(0).toString().trim().split('\n');
+let [a,...x] = aX;
+a = a.split(' ').map(Number);
+x = +x[0];
+const getGCMSet = (n)=>{
+    const gcmSet = new Set([1,n]);
+    for(let i=2;i<=n/2;i++){
+        if(n%i===0){
+            gcmSet.add(i);
+        }
+    }
+    return gcmSet;
+}
+
+const isCoprime = (setA,setB)=>{
+    const filtered = [...setA].filter((v)=>setB.has(v));
+    return filtered.length===1;
+} 
+
+const setB = getGCMSet(x);
+const coprimes = a.filter(v=>isCoprime(getGCMSet(v),setB));
+const sum = coprimes.reduce((acc,curr)=>acc+=curr,0);
+
+console.log((sum/coprimes.length).toFixed(2));
