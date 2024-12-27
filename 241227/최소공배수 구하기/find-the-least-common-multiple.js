be3c17df9d4ae7bc02d283998b3de02d9405609d
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2024-12-27 15:38:00 +0900

    Title: 최소공배수 구하기, Time: 190ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241227/최소공배수 구하기/find-the-least-common-multiple.js
+++ 241227/최소공배수 구하기/find-the-least-common-multiple.js
@@ -1,20 +1,60 @@
 const fs = require('fs');
 const [n,m] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 
-const getGCM = (n) => {
-    const gcm = [];
-    for(let i=2;i<n;i++){
-        if(n%i===0){
-            gcm.push(i);
+// 48 15
+//  2,  3,  2*2,  2*3, 2*2*2, 2*2*3, 2*2*2*2, 2*2*2*3 
+//  3,  5 
+// 240 = 2*2*2*2*3*5
+
+// 두 수에 서로 공통으로 존재하는 배수 중 가장 작은 수
+const gcmCount = (n,gcmNum)=> {
+    let count = 0;
+    while(n/gcmNum>0){
+        if(n%gcmNum===0){
+             n/=gcmNum;
+             count++;
+        }else {
+            break;
+        }
+    }
+    return count;
+}
+
+const getGCM = (N) =>{
+    const gcm = {};
+    let copy = N;
+    let i = 3;
+    if(N%2===0){
+        gcm['2'] = gcmCount(copy,2);
+        copy /= ((gcmCount(copy,2))*2);
+    }
+    while(true){
+        if(copy%i===0){
+            gcm[i] = gcmCount(copy,i);
+            copy /= ((gcmCount(copy,i))*i);
+        }
+        i++;
+        if(copy<i){
+            break;
         }
     }
     return gcm;
 }
 
-const nGCM = getGCM(n);
-const mGCM = new Set(getGCM(m));
+const nGcmObj = getGCM(n);
+const mGcmObj = getGCM(m);
 
-// 두 수에 서로 공통으로 존재하는 배수 중 가장 작은 수
-const lcm = nGCM.filter(v=>mGCM.has(v)).reduce((acc,curr)=>acc*=curr,1);
+let lcm = 1;
+const keys = [...new Set([...Object.keys(nGcmObj),...Object.keys(mGcmObj)])];
+
+for(let key of keys){
+    const nGcmK= nGcmObj[key];
+    const mGcmK= mGcmObj[key];
+    if(nGcmK!=null && mGcmK!=null){
+        lcm*=key**Math.max(nGcmK,mGcmK);
+    } else {
+        lcm*=nGcmK!=null? (key**nGcmK):key**mGcmK;
+    }
+}
 
 console.log(lcm);
