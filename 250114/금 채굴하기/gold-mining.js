08278db53c5d9312eaf862ec78f8e9242a9b6a0a
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-01-14 15:45:34 +0900

    Title: 금 채굴하기, Time: 227ms, Memory: 19MB, Status: Passed - CodeTree

--- 250114/금 채굴하기/gold-mining.js
+++ 250114/금 채굴하기/gold-mining.js
@@ -2,61 +2,62 @@ const fs = require('fs');
 
 const [nm,...nums]= fs.readFileSync(0).toString().trim().split('\n');
 const [n,m] = nm.split(' ').map(Number);
-const mine = nums.map(v=>v.split(' ').map(Number));
-
+const matrix = nums.map(v=>v.split(' ').map(Number));
 
 const checkRange = (x, y) => x >= 0 && y >= 0 && x < n && y < n;
 
-  const getGold = (arr) => {
-    let cntGold = 0;
-    arr.forEach(([x, y]) => {
-      if (mine[x][y] === 1) cntGold++;
-    });
-    return cntGold;
-  };
-
-  const bfs = (queue, visited, currK) => {
-    const dx = [1, -1, 0, 0];
-    const dy = [0, 0, 1, -1];
-    const bfsList = [];
-
-    while (queue.length > 0) {
-      const [x, y, k] = queue.shift();
-
-      for (let d = 0; d < 4; d++) {
-        const nx = x + dx[d];
-        const ny = y + dy[d];
-        if (checkRange(nx, ny) && !visited[nx][ny] && k + 1 <= currK) {
-          queue.push([nx, ny, k + 1]);
-          visited[nx][ny] = true;
-          bfsList.push([nx, ny, k + 1]);
-        }
-      }
+const getGold = (arr) => {
+  let cntGold = 0;
+  arr.forEach(([x, y]) => {
+    if (matrix[x][y] === 1){
+        cntGold++;
     }
-    return bfsList;
-  };
+  });
+  return cntGold;
+};
 
-  let answer = 0;
+const bfs = (queue, visited, currK) => {
+  const dx = [1, -1, 0, 0];
+  const dy = [0, 0, 1, -1];
+  const bfsList = [...queue.map(([x, y]) => [x, y])]; 
 
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      for (let kk = 0; kk <= n; kk++) {
-        const currK = kk;
-        const visited = Array.from({ length: n }, () => Array(n).fill(false));
-        const center = [i, j, 0];
-        visited[i][j] = true;
+  while (queue.length > 0) {
+    const [x, y, k] = queue.shift();
 
-        const queue = [center];
-        const bfsList = bfs(queue, visited, currK);
-
-        const cost = currK * currK + (currK + 1) * (currK + 1);
-        const earn = getGold(bfsList);
-
-        if (cost <= earn * m) {
-          answer = Math.max(answer, earn);
-        }
+    for (let d = 0; d < 4; d++) {
+      const nx = x + dx[d];
+      const ny = y + dy[d];
+      if (checkRange(nx, ny) && !visited[nx][ny] && k + 1 <= currK) {
+        queue.push([nx, ny, k + 1]);
+        visited[nx][ny] = true;
+        bfsList.push([nx, ny]);
       }
     }
   }
+  return bfsList;
+};
+
+let answer = 0;
+
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < n; j++) {
+    for (let kk = 0; kk <= n; kk++) {
+      const currK = kk;
+      const visited = Array.from({ length: n }, () => Array(n).fill(false));
+      const center = [i, j, 0];
+      visited[i][j] = true;
+
+      const queue = [center];
+      const bfsList = bfs(queue, visited, currK);
+
+      const cost = currK * currK + (currK + 1) * (currK + 1);
+      const earn = getGold(bfsList);
+
+      if (cost <= earn * m) {
+        answer = Math.max(answer, earn);
+      }
+    }
+  }
+}
 
   console.log(answer);
