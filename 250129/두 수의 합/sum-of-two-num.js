d6133424226a26d39fba564e48eb9d14ed5cc003
Hye min Pyo <102423086+Pyotato@users.noreply.github.com>
2025-01-29 23:13:36 +0900

    Title: 두 수의 합, Time: 178ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250129/두 수의 합/sum-of-two-num.js
+++ 250129/두 수의 합/sum-of-two-num.js
@@ -2,15 +2,19 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
 const [n,k] = input.shift().split(" ").map(Number);
 const nums = input[0].split(' ').map(Number);
+const map = new Map();
+
+nums.forEach((v,i) => map.has(v)? map.set(v,[...map.get(v),i]):map.set(v,[i]))
 
 const set = new Set();
 
-for(let i=0;i<n;i++){
-    for(let j=i+1;j<n;j++){
-        if(nums[i] + nums[j]===k){
-            set.add(`(${i}, ${j})`);
-        }
+let count = 0;
+
+for(let key of map.keys()){
+    const other = k-key;
+    if(map.has(other) && !set.has([other,key].sort().join(' '))){
+        set.add([other,key].sort().join(' '));
+        count+=(map.get(key).length*map.get(other).length);
     }
 }
-
-console.log(set.size);
+console.log(count);
